2d33ac8c75ecb5c96257cf1808f2e5a6fa79efd0
Алексей Хабиров <aleksejhabirov@MacBook-Air-Aleksej.local>
2021-03-04 18:43:26 +0300

    feat: add two solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.length) return [];
+  
+  return members
+    .filter(member => typeof member === 'string')
+    .map(member => {
+      const adaptedMember = member.trim().toUpperCase();
+      return adaptedMember[0];
+    })
+    .sort()
+    .join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const seasons = {
+  winter: [11, 0, 1],
+  spring: [2, 3, 4],
+  summer: [5, 6, 7],
+  fall: [8, 9, 10]
+};
+
+module.exports = function getSeason(date) {
+  if (typeof date === 'undefined') return 'Unable to determine the time of year!';
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('Invalid argument');
+
+  const month = date.getMonth();
+
+  for (const season in seasons) {
+    if (seasons[season].includes(month)) {
+      return season;
+    };
+  }
 };
